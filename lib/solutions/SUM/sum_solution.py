f94a0b0fd0a39ca93b25c461e5b3937977937ed2
sbx_user1051 <sbx_user1051@169.254.51.213>
2019-03-23 20:07:58 +0000

    Sat Mar 23 20:07:58 UTC 2019

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
-    raise NotImplementedError()
+	'sum the values x and y'
+	return x + y
+	# i assume there may be some invalid inputs. 
+	# will try running and check
+    #raise NotImplementedError()
+
