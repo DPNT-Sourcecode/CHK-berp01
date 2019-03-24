75c4de05499c6f961cfc6bc81afb4f0296b4a7b7
sbx_user1060 <sbx_user1060@ip-10-128-242-216.eu-west-2.compute.internal>
2019-03-24 17:38:18 +0000

    Sun Mar 24 17:38:18 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,55 +1,61 @@
 
 
 def get_sku_lookup():  # normally would have to query a database or something, will cheat and return a dict
-	''' get a dict of all the {skus:prices}'''
-	sku_lookup = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-	return sku_lookup;
+    ''' get a dict of all the {skus:prices}'''
+    sku_lookup = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    return sku_lookup
 
 
 def get_special_offers():
 
-	# quickly work out the differencs of things.
-	#
-	# special_offers = {'A':{3: -20}, {'B':{2, -15}}} # for every  {x:{y:z}} for every y of x add z
-	# special_offers = {'A':{3: 130}, {'B':{2, 45}}} // for every  {x:{y:z}} for every y of x add z
-	special_offers = {'A': [3, 130], 'B': [2, 45]}
-	return special_offers
+    # quickly work out the differencs of things.
+    #
+    # special_offers = {'A':{3: -20}, {'B':{2, -15}}} # for every  {x:{y:z}} for every y of x add z
+    # special_offers = {'A':{3: 130}, {'B':{2, 45}}} // for every  {x:{y:z}} for every y of x add z
+    special_offers = {'A': [3, 130], 'B': [2, 45]}
+    return special_offers
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 
 
 def checkout(skus):  # TO DO : Optimise
-	'supermarket checkout caluclator, returns cost of items skus'
-	# illegal input return -1
-	# caluclate the value of item factoring in special offers.
+    'supermarket checkout caluclator, returns cost of items skus'
+    # illegal input return -1
+    # caluclate the value of item factoring in special offers.
 
-	# we actually want to keep a count of the different items
+    # we actually want to keep a count of the different items
 
-	prices = get_sku_lookup()
-	# easier to read than count array
-	items_count = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
-	basket = {}
+    prices = get_sku_lookup()
+    # easier to read than count array
+    items_count = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
+    basket = {}
 
-	for item in skus:
-		if item not in items_count.keys():
-			return -1
-		items_count[item] = items_count[item] + 1;
+    for item in skus:
+        #item = upper(item)
+        if item not in items_count.keys():
+            return -1
+        items_count[item] = items_count[item] + 1
 
-	total = 0
-	for item in set(skus):
-		if item in special_offers().keys() and items_count[item] >= special_offers()[item][0]:
-			# calculate the prices for every amount of items that reach the deal.
-			# if we have 7 items, and every 3 have a speial price
-			# 7 / 3  = 2, so its 2* special price, then the remainder * the original price.
-			total = total + (special_offers[item][1] * int(items_count[item]/special_offers[item][0]))
-			total = total + ((items_count[item] % special_offers[item][0]) * prices[item])
-		else:
-			total=total + (items_count[item] * prices[item])
-	return total;
+    special_offers = get_special_offers()
+    total = 0
+    for item in set(skus):
+        if item in special_offers.keys() and items_count[item] >= special_offers[item][0]:
+            # calculate the prices for every amount of items that reach the deal.
+            # if we have 7 items, and every 3 have a speial price
+            # 7 / 3  = 2, so its 2* special price, then the remainder * the original price.
+            total = total + \
+                (special_offers[item][1] *
+                 int(items_count[item]/special_offers[item][0]))
+            total = total + \
+                ((items_count[item] % special_offers[item][0]) * prices[item])
+        else:
+            total = total + (items_count[item] * prices[item])
+    return total
+
 
 # tests
 if __name__ == "__main__":
-	print(checkout(['A','A','A']))
-	
-
+    print(checkout(['A', 'A', 'A']) == 130)
+    print(checkout(['A', 'A', 'A', 'a']) == -1)
+    print(checkout(['A', 'A', 'A', 'B', 'B']) == 175)
