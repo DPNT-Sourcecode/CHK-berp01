bf3abf0eb8695f375885a40419d01ca52b9ee686
sbx_user1051 <sbx_user1051@169.254.200.117>
2019-03-26 22:27:31 +0000

    Tue Mar 26 22:27:31 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,7 +13,33 @@ def get_basket(sku_string):
 
 def get_sku_lookup():  # normally would have to query a database or something, will cheat and return a dict
     ''' get a dict of all the {skus:prices}'''
-    sku_lookup = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
+    sku_lookup = {
+ A : 50,
+B : 30,
+C : 20,
+D : 15,
+E : 40,
+F : 10,
+G : 20,
+H : 10,
+I : 35,
+J : 60,
+K : 80,
+L : 90,
+M : 15,
+N : 40,
+O : 10,
+P : 50,
+Q : 30,
+R : 50,
+S : 30,
+T : 20,
+U : 40,
+V : 50,
+W : 20,
+X : 90,
+Y : 10,
+Z : 50}
     return sku_lookup
 
 
